fbd39f6838149e98f7db1e07296102d709c36cc2
David Almeida <contato.davidalmeida@outlook.com>
2021-11-29 15:08:35 -0300

    ADD DEFAULT COMMIT

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Apenas um disco pode ser movido por vez. 
+// => IMPEDIR QUE O USUÀRIO CLIQUE EM MAIS DE UM DISCO POR TURNO/JOGADA.
+
+// Cada movimento consiste de pegar o disco de cima de uma das pilhas e movê-lo para o topo de outra pilha.
+// => DOIS CLIQUES, TORRE DE ORIGEM E TORRE DE DESTINO.
+
+// Nenhum disco pode ser colocado no topo de um disco menor.
+// => CONDIÇÂO PARA COMPARAR DISCO MAIOR E MENOR, MAIOR NÂO PODE FICAR EM CIMA DO MENOR.
+
