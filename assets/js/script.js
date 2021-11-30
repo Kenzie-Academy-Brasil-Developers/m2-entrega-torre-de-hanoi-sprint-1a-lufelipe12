9a90b4571e3e310896ee98cd562cb2398bb61646
David Almeida <contato.davidalmeida@outlook.com>
2021-11-29 22:51:07 -0300

    Creating new elements by DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,29 @@
 
 // Nenhum disco pode ser colocado no topo de um disco menor.
 // => CONDIÇÂO PARA COMPARAR DISCO MAIOR E MENOR, MAIOR NÂO PODE FICAR EM CIMA DO MENOR.
+
+// SECTION
+const main = document.getElementById('main');
+
+// TORRE
+const towerOne = document.createElement('section');
+towerOne.className = "tower";
+const towerTwo = document.createElement('section');
+towerTwo.className = "tower";
+const towerThree = document.createElement('section');
+towerThree.className = "tower";
+
+// DISCOS
+const discRed = document.createElement('div'); // DISCO 1 - RED
+discRed.className = "discRed";
+const discGreen = document.createElement('div'); // DISCO 2 - YELLOW
+discGreen.className = "discGreen";
+const discBlue = document.createElement('div'); // DISCO 3 - BLUE
+discBlue.className = "discBlue";
+
+// ADD TOWERS INTO MAIN
+main.appendChild(towerOne);
+main.appendChild(towerTwo);
+main.appendChild(towerThree);
+
+// ADD DISCS INTO TOWERS
