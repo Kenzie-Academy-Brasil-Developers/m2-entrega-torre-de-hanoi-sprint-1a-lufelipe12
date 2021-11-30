7d91504a3f4d9ff46fe9e6b0f882138c47b9bf65
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2021-11-29 23:01:09 -0300

    Add functions for dificulty level and buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,44 @@
 
 // Nenhum disco pode ser colocado no topo de um disco menor.
 // => CONDIÇÂO PARA COMPARAR DISCO MAIOR E MENOR, MAIOR NÂO PODE FICAR EM CIMA DO MENOR.
+
+
+
+const easyLevel = () => {
+    towerOne.appendChild(discRed)
+    towerOne.appendChild(discGreen)
+    towerOne.appendChild(discBlue)
+    main.appendChild(resetButton)
+}
+
+const mediumLevel = () => {
+    towerOne.appendChild(discRed)
+    towerOne.appendChild(discGreen)
+    towerOne.appendChild(discBlue)
+    towerOne.appendChild(discYellow)
+    main.appendChild(resetButton)
+
+}
+
+const hardLevel = () => {
+    towerOne.appendChild(discRed)
+    towerOne.appendChild(discGreen)
+    towerOne.appendChild(discBlue)
+    towerOne.appendChild(discYellow)
+    towerOne.appendChild(discOrange)
+    main.appendChild(resetButton)
+
+}
+
+
+const easyButton = document.getElementById('easy')
+const mediumButton = document.getElementById('medium')
+const hardButton = document.getElementById('hard')
+
+easyButton.addEventListener('click', easyLevel)
+mediumButton.addEventListener('click', mediumLevel)
+hardButton.addEventListener('click', hardLevel)
+
+
+
+
